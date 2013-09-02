3da5ea9b0039e803a9c35be9f4e5cd047deb8eed
Javier Constain <constainopia@gmail.com>
2013-09-02 17:15:45 -0500

    Ya puedo usar los ints del excel
    Era un problema con el origen del for, al parecer. Ahora sigue procesar
    todo el archivo

--- Borradores/codigo/validacion_matriz_excel_excel.py
+++ Borradores/codigo/validacion_matriz_excel_excel.py
@@ -1,29 +1,40 @@
+# -*- coding: utf-8 -*-
 import xlrd
+from decimal import Decimal
+#import repr
+import sys
 
 def cruza_campos(parametro, validado):
     return parametro[3]
 
 def cruzados(parametro, validado, n):
-    ccol_n = parametro.cell(n, 8)
-    rrowx= round(ccol_n.value)
-    #rrowx = parametro.cell_value(rowx=n,colx =8)
-    #rrowx = parametro.cell_type(rowx=n,colx =8) 
+    rrow_n = parametro.cell(n, 8) 
+    rrowx= (rrow_n.value)
     ccolx = parametro.cell_value(rowx=n,colx =9)
-    print ('%s, %s')%((rrowx),type(ccolx))
-    return "" #parametro.cell_value(rowx=rrowx, colx = ccolx)
+    return validado.cell_value(colx=int(rrowx), rowx = int(ccolx))
 
+# Que las estratégias de participación se definan después de la etapa de aprestamiento.
+# Que los costos los asuma cada CAR
+#En todo pomca debe haber consejo de cuenca
+#Diagnóstico, la misma matriz permite llenar la información que se requiere, yo filtro
+#variabilidad climática
 matrizfile = "C:/Users/ASOCARS-FADAPT3/Documents/GitHub/POMCAS/Borradores/Matrices/matriz institucional  17 de Agosto.xls"
 validafile = "C:/Users/ASOCARS-FADAPT3/Documents/GitHub/POMCAS/Borradores/Parametrizacion/Estructura de las matrices.xlsx"
 
-book = xlrd.open_workbook(matrizfile)
+book = xlrd.open_workbook(matrizfile)#, encoding_override="unicode")
 pestana = book.sheet_by_index(1)
 
-campos = xlrd.open_workbook(validafile).sheet_by_index(2)
+campos = xlrd.open_workbook(validafile).sheet_by_index(2)  #, encoding_override="unicode")
+
+for fila in range(3,18):
+    print  cruzados(campos, pestana, fila)
 
 
-for fila in range(1,18):
-    print cruzados(campos, pestana, fila)
-#    print cruza_campos(campos.row(fila), pestana)
+pestana = book.sheet_by_index(2)
+print pestana.cell_value(colx=int(rrowx), rowx = int(ccolx))
+
+#for fila in range(19,23):
+#    print  cruzados(campos, pestana, fila)
 
 #print book.sheet_names()
 #print len(book.sheet_names())
