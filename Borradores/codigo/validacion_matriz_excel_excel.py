4e7b7ba69b2b7478b57a0e99cc53cbdbbaf5eb4e
Javier Constain <constainopia@gmail.com>
2013-09-03 17:19:06 -0500

    Parametrizando matriz
    Matriz de una corpo corregida.
    Matriz de actividades consolidada.

--- Borradores/codigo/validacion_matriz_excel_excel.py
+++ Borradores/codigo/validacion_matriz_excel_excel.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 import xlrd
-from decimal import Decimal
+#from decimal import Decimal
 #import repr
-import sys
+#import sys
 
 def cruza_campos(parametro, validado):
     return parametro[3]
 
 def cruzados(parametro, validado, n):
     rrow_n = parametro.cell(n, 8) 
-    rrowx= (rrow_n.value)
-    ccolx = parametro.cell_value(rowx=n,colx =9)
-    return validado.cell_value(colx=int(rrowx), rowx = int(ccolx))
+    ccolx= (rrow_n.value)
+    rrowx = parametro.cell_value(rowx=n,colx =9)
+    return validado.cell_value(colx=int(ccolx), rowx = int(rrowx))
 
 # Que las estratégias de participación se definan después de la etapa de aprestamiento.
 # Que los costos los asuma cada CAR
@@ -21,20 +21,19 @@ def cruzados(parametro, validado, n):
 matrizfile = "C:/Users/ASOCARS-FADAPT3/Documents/GitHub/POMCAS/Borradores/Matrices/matriz institucional  17 de Agosto.xls"
 validafile = "C:/Users/ASOCARS-FADAPT3/Documents/GitHub/POMCAS/Borradores/Parametrizacion/Estructura de las matrices.xlsx"
 
-book = xlrd.open_workbook(matrizfile)#, encoding_override="unicode")
+book = xlrd.open_workbook(matrizfile)
 pestana = book.sheet_by_index(1)
 
-campos = xlrd.open_workbook(validafile).sheet_by_index(2)  #, encoding_override="unicode")
+campos = xlrd.open_workbook(validafile).sheet_by_index(2)  
 
 for fila in range(3,18):
     print  cruzados(campos, pestana, fila)
 
 
 pestana = book.sheet_by_index(2)
-print pestana.cell_value(colx=int(rrowx), rowx = int(ccolx))
 
-#for fila in range(19,23):
-#    print  cruzados(campos, pestana, fila)
+for fila in range(26,92):
+    print  cruzados(campos, pestana, fila)
 
 #print book.sheet_names()
 #print len(book.sheet_names())
@@ -42,8 +41,8 @@ print pestana.cell_value(colx=int(rrowx), rowx = int(ccolx))
 #for i in range (10):
 # print book.sheet_by_index(i).name
  
-#for rx in range(campos.nrows):
-#    print campos.row(rx)
+#for rx in range(pestana.nrows):
+#    print pestana.row(rx)
 #for i in range(len(campos.sheet_names())):
 # print campos.sheet_by_index(i).name
  
