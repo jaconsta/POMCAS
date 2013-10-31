234f4aa7db4284455215e905be796905c8391b5d
jaconsta <constainopia@gmail.com>
2013-10-31 12:19:07 -0500

    Improved or made looks nicer The file load module. Added infoidentif module More urls

--- asocarssgi/regist_carga/views.py
+++ asocarssgi/regist_carga/views.py
@@ -4,165 +4,118 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 
+import re
+
 from django.contrib.auth.models import User
 
 from asocarssgi import default_names
 from .forms import UploadFileForm, UploadMCartogForm
 from .models import *
 
+def ValidFileName(filename, matrix, exten):
+    #REGEX1: CTO85_\d{4}\d{1,2}\d{1,2}_MATRIZ_%s_(?P<corpora>\w+).(?P<fileext>\w{3,4})
+    #REGEX2: CTO85_(?<year>20[1-9]\d)(?<month>0?[1-9]|1[012])(?<day>0?[1-9]|[12]\d|3[01])_MATRIZ_%s_(?<corpora>\w+).(?<fileext>\w{3,4})
+    pattern = re.compile('CTO85_(?<year>20[1-9]\d)(?<month>0?[1-9]|1[012])(?<day>0?[1-9]|[12]\d|3[01])_MATRIZ_%s_(?<corpora>\w+).(?<fileext>\w{3,4})'%(matrix))
+    match = pattern.match(pattern, filename)
+    if not match:
+        output = (u'El nombre de archivo (%s) no es correcto \n'%(filename))
+        return (False, output)
+    elif match(fileext) not in exten:
+        output = (u'La extensión de archivo (%s) no es correcta \n'%(fileext))
+        output += (u'Se esperaba %s'%(exten))
+        return (False, output)
+    output = fileext
+    return (True, output)
+
+def FileInTail(request, matrix):
+    '''
+    Returns False if there is the user has already uploaded a file.
+    Pending to process or processed and approved
+    '''
+    if archivosb.objects.filter(
+            archnomb = matrix, 
+            archuser = request.user,
+            archesta__in = ['REC', 'APR']
+        ).count() ==0:
+        return False
+    return True
+
+def SetFile(matrix, upfile):
+    try:
+        max_id = archivosb.objects.all().order_by("-id")[0].id
+    except IndexError:
+        max_id = 0
+
+    name = upfile.name
+    upfile.name = '%d.%s'%((max_id+1), sop_ext)
+    loaded = archivosb(
+        archnomb = matrix,
+        archubic = upfile,
+        archuser = request.user,
+        archorna = name,
+        archesta = 'REC'
+    )
+    loaded.save()
+    return loaded
+
+def GetExcelExt():
+    return ['xls', 'xlsx']
+
+def GetComprExt():
+    return ['rar', 'zip', 'tar', 'tar.gz', '7z']
+
 @login_required(login_url = ('%slogin/' %(default_names.SUB_SITE)))
-def modelo(request):
+def RecolCartografic(request):
+    title = u'Matriz de Cartografía Básica'
+    if not FileInTail(request, 'MATRIZ_CARTOGRAFICA'):
+        message = u'Ya ha realizado el cargue de esta matriz'
+        return render_to_response('matriz_recibida.html', {'errors': message})
     if request.method == 'POST':
         error = ''
         form = UploadMCartogForm(request.POST, request.FILES)
         if form.is_valid():
             #Parse Matrix name
             arch_name = request.FILES['archivo'].name
-            arch_name = arch_name.split('.')
-            arch_ext = arch_name[-1] #File Extension
-            arch_name_p = arch_name[0].split('_') #File name components
-            if len(arch_name_p) != 5:
-                error += 'El nombre de archivo no parece correcto \n'
-                error += '%s, %s\n'%(arch_name_p, len(arch_name_p))
-            elif arch_ext not in ['xls', 'xlsx']:
-                error += 'La extensión del archivo debe ser un archivo de Excel \n'
-		
-		error += '%s\n'%(arch_name_p)
+            [parsed, message] = ValidFileName(arch_name, 'CARTOGRAFICA', GetExcelExt())
+            if not parsed:
+                return render_to_response('matriz_recibida.html', {'errors': message})
+
             #Upload The Matrix
-            try:
-                max_id = archivosb.objects.all().order_by("-id")[0].id
-            except IndexError:
-                max_id = 0
-            archives = request.FILES['archivo']
-            nombre = archives.name
-            archives.name = '%d.%s'%((max_id+1), arch_ext)
-            creado = archivosb(
-                archnomb = 'MATRIZ_CARTOGRAFICA',
-                archubic = archives,
-                archuser = request.user,
-                archorna = nombre, #request.FILES['archivo'].name,
-                archesta = 'REC'
-            )
-            creado.save()
+            SetFile('MATRIZ_CARTOGRAFICA', request.FILES['archivo'])
             return render_to_response('matriz_recibida.html', {'errors': error}) 
     else: 
         form = UploadMCartogForm()
-    return render_to_response('cargue_archivos.html', {'form':form}, context_instance =RequestContext(request))
+        title = u'Matriz de Cartografía básica'
+    return render_to_response('cargue_archivos.html', {'form':form, 'title': title}, context_instance =RequestContext(request))
 
-#    if request.method == 'POST':
-#        error = ''
-#        form = UploadFileForm(request.POST, request.FILES)
-#        if form.is_valid():
-#            #Parse Matrix name
-#            arch_name = request.FILES['archivo'].name
-#            arch_name = arch_name.split('.')
-#            arch_ext = arch_name[-1] #File Extension
-#            arch_name_p = arch_name[0].split('_') #File name components
-#            if len(arch_name_p) != 6:
-#                error += 'El nombre de archivo no parece correcto'
-#            elif arch_ext not in ['xls', 'xlsx']:
-#                error += 'La extensión del archivo debe ser un archivo de Excel'
-#            #Parse attached files
-#            sop_name = request.FILES['soportes'].name
-#            sop_name= sop_name.split('.')
-#            sop_ext = sop_name[-1] #File Extension
-#            sop_name_p = sop_name[0].split('_') #File name components
-#            if len(sop_name_p) != 6:
-#                error += 'El nombre de archivo no parece correcto'
-#            elif sop_ext not in ['rar', 'zip']:
-#                error += 'La extensión del archivo debe ser un archivo de rar o ip'
-#            #Upload The Matrix
-#            try:
-#                max_id = archivosb.objects.all().order_by("-id")[0].id
-#            except IndexError:
-#                max_id = 0
-#            archives = request.FILES['archivo']
-#            archives.name = '%d.%s'%((max_id+1), arch_ext)
-#            creado = archivosb(
-#                archnomb = 'MATRIZ_INSTITUCIONAL',
-#                archubic = archives,
-#                archuser = request.user,
-#                archorna = request.FILES['archivo'].name,
-#                archesta = 'REC'
-#            )
-#            attacheds = request.FILES['soportes']
-#            attacheds.name = '%d.%s'%((max_id+2), sop_ext)
-#            creadodos = archivosb(
-#                archnomb = 'MATRIZ_INSTITUCIONAL_SOP',
-#                archubic = attacheds,
-#                archuser = request.user,
-#                archorna = request.FILES['soportes'].name,
-#                archesta = 'REC'
-#            )
-#            #if creado.is_valid():
-#            creado.save()
-#            #else: 
-#            #error = 'Hay errores en la matriz'
-#            #if creadodos.is_valid():
-#            creadodos.save()
-#            #else: 
-#            #error = 'Hay errores en los soportes'
-#            #form.save()
-#            return render_to_response('matriz_recibida.html', {'errors': error}) #HttpResponseRedirect('/proyecto85/b')
-#    else: 
-#        form = UploadFileForm()
-#    return render_to_response('cargue_archivos.html', {'form':form}, context_instance =RequestContext(request))
-#    if request.method == 'POST':
-#        error = ''
-#        form = UploadFileForm(request.POST, request.FILES)
-#        if form.is_valid():
-#            #Parse Matrix name
-#            arch_name = request.FILES['archivo'].name
-#            arch_name = arch_name.split('.')
-#            arch_ext = arch_name[-1] #File Extension
-#            arch_name_p = arch_name[0].split('_') #File name components
-#            if len(arch_name_p) != 6:
-#                error += 'El nombre de archivo no parece correcto'
-#            elif arch_ext not in ['xls', 'xlsx']:
-#                error += 'La extensión del archivo debe ser un archivo de Excel'
-#            #Parse attached files
-#            sop_name = request.FILES['soportes'].name
-#            sop_name= sop_name.split('.')
-#            sop_ext = sop_name[-1] #File Extension
-#            sop_name_p = sop_name[0].split('_') #File name components
-#            if len(sop_name_p) != 6:
-#                error += 'El nombre de archivo no parece correcto'
-#            elif sop_ext not in ['rar', 'zip']:
-#                error += 'La extensión del archivo debe ser un archivo de rar o ip'
-#            #Upload The Matrix
-#            try:
-#                max_id = archivosb.objects.all().order_by("-id")[0].id
-#            except IndexError:
-#                max_id = 0
-#            archives = request.FILES['archivo']
-#            archives.name = '%d.%s'%((max_id+1), arch_ext)
-#            creado = archivosb(
-#                archnomb = 'MATRIZ_INSTITUCIONAL',
-#                archubic = archives,
-#                archuser = request.user,
-#                archorna = request.FILES['archivo'].name,
-#                archesta = 'REC'
-#            )
-#            attacheds = request.FILES['soportes']
-#            attacheds.name = '%d.%s'%((max_id+2), sop_ext)
-#            creadodos = archivosb(
-#                archnomb = 'MATRIZ_INSTITUCIONAL_SOP',
-#                archubic = attacheds,
-#                archuser = request.user,
-#                archorna = request.FILES['soportes'].name,
-#                archesta = 'REC'
-#            )
-#            #if creado.is_valid():
-#            creado.save()
-#            #else: 
-#            #error = 'Hay errores en la matriz'
-#            #if creadodos.is_valid():
-#            creadodos.save()
-#            #else: 
-#            #error = 'Hay errores en los soportes'
-#            #form.save()
-#            return render_to_response('matriz_recibida.html', {'errors': error}) #HttpResponseRedirect('/proyecto85/b')
-#    else: 
-#        form = UploadFileForm()
-#    return render_to_response('cargue_archivos.html', {'form':form}, context_instance =RequestContext(request))
-#        
+@login_required(login_url = ('%slogin/' %(default_names.SUB_SITE)))
+def RecolInstitucional(request):
+    title = u'Matriz de Diagnóstico institucional'
+    if not FileInTail(request, 'MARTIZ_INSTITUCIONAL'):
+        message = u'Ya ha realizado el cargue de esta matriz'
+        return render_to_response('matriz_recibida.html', {'title': title, 'errors': message})
+    if request.method == 'POST':
+        error = ''
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            #Parse Matrix name
+            arch_name = request.FILES['archivo'].name
+            [parsed, message] = ValidFileName(arch_name, 'INSTITUCIONAL', GetExcelExt())
+            error += message
+            if not parsed:
+                return render_to_response('matriz_recibida.html', {'errors': message}) #HttpResponseRedirect('/proyecto85/b')
+            arch_ext = message
+            #Parse attached files
+            sop_name = request.FILES['soportes'].name
+            [parsed, message] = ValidFileName(sop_name, 'INSTITUCIONAL_SOPORTES', GetComprExt())
+            error += message
+            if not parsed:
+                return render_to_response('matriz_recibida.html', {'errors': message})
+            sop_ext = message
+            #Upload The Matrices
+            SetFile('MATRIZ_INSTITUCIONAL', request.FILES['archivo'])
+            SetFile('MATRIZ_INSTITUCIONAL_SOP', request.FILES['soportes'])
+            return render_to_response('matriz_recibida.html', {'errors': error})
+    else: 
+        form = UploadFileForm()
+    return render_to_response('cargue_archivos.html', {'form':form, 'title':title}, context_instance =RequestContext(request))
