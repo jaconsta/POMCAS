c7b55c069d69c5fd40ccbff2ce65d9b9d7de1e9d
jaconsta <japomcas@debian.http://www.asocars.org.co/testenv>
2014-02-03 09:55:48 -0500

    mapexte was missing

---
+++ asocarssgi/static/js/jquery.init.js
@@ -0,0 +1,6 @@
+/* Puts the included jQuery into our own namespace using noConflict and passing
+ * it 'true'. This ensures that the included jQuery doesn't pollute the global
+ * namespace (i.e. this preserves pre-existing values for both window.$ and
+ * window.jQuery).
+ */
+var jquery = $.noConflict();
